059ccc517c0c082e882e4ac51baaff1862f86153
Heechul Ryu <ryuhcii@gmail.com>
2023-02-05 15:04:20 -0800

    Lint and format by eslint and prettier

--- src/components/CounterWrapper.svelte
+++ src/components/CounterWrapper.svelte
@@ -2,11 +2,10 @@
 A counter written with Svelte
 -->
 <script lang="ts">
-  import SvelteCounter from './SvelteCounter.svelte'
+  import SvelteCounter from './SvelteCounter.svelte';
   const handler = () => alert("this works because it's still coming froms svelte");
-	
 </script>
 
-<SvelteCounter handler={handler}>
+<SvelteCounter {handler}>
   <h1>Counter is wrapped and a function prop is passed from Svelte Wrapper!</h1>
 </SvelteCounter>
--- src/components/SvelteCounter.svelte
+++ src/components/SvelteCounter.svelte
@@ -2,24 +2,24 @@
 A counter written with Svelte
 -->
 <script lang="ts">
-  export let handler = () => alert("internally owned handler");
-	let count = 0;
+  export let handler = () => alert('internally owned handler');
+  let count = 0;
 
-	function add() {
-		count += 1;
-	}
+  function add() {
+    count += 1;
+  }
 
-	function subtract() {
-		count -= 1;
-	}
+  function subtract() {
+    count -= 1;
+  }
 </script>
 
 <div class="counter">
-	<button on:click={subtract}>-</button>
-	<pre>{count}</pre>
-	<button on:click={add}>+</button>
-	<button on:click={handler}>=</button>
+  <button on:click={subtract}>-</button>
+  <pre>{count}</pre>
+  <button on:click={add}>+</button>
+  <button on:click={handler}>=</button>
 </div>
 <div class="counter-message">
-	<slot />
+  <slot />
 </div>
